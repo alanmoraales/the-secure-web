367c3a4f64fb26de3bd59d46b9392070b27a77c4
Alan Morales <alanmoraales845@gmail.com>
2021-11-28 14:22:30 -0600

    removed gallery preview section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,24 @@
-import { GetStaticProps, InferGetStaticPropsType } from 'next'
 import { Grid } from '@chakra-ui/react'
-import { getPlaiceholder } from 'plaiceholder'
 import PageTemplate from '@templates/PageTemplate'
 import LatestContentSection from '@organisms/LatestContentSection'
-import GalleryPreviewSection from '@organisms/GalleryPreviewSection'
 import Head from '@atoms/Head'
-import images from 'data/galleryImages'
 import seoMetadata from 'data/seoMetadata'
 
-export const getStaticProps: GetStaticProps = async () => {
-  const previewGalleryImages = await Promise.all(
-    images.slice(0, 7).map(async (image) => {
-      const { css } = await getPlaiceholder(image.smallJpg)
-      return {
-        ...image,
-        css,
-      }
-    })
-  )
-
-  return {
-    props: {
-      previewGalleryImages,
-    },
-  }
-}
-
-const Home = ({
-  previewGalleryImages,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
-  return (
-    <>
-      <Head seoMetadata={seoMetadata.home} />
-      <PageTemplate>
-        <Grid
-          w="100%"
-          templateColumns={{ base: 'auto', lg: '65% auto' }}
-          gap={16}
-        >
-          <Grid gap={16}>
-            <LatestContentSection />
-          </Grid>
+const Home = () => (
+  <>
+    <Head seoMetadata={seoMetadata.home} />
+    <PageTemplate>
+      <Grid
+        w="100%"
+        templateColumns={{ base: 'auto', lg: '65% auto' }}
+        gap={16}
+      >
+        <Grid gap={16}>
+          <LatestContentSection />
         </Grid>
-        <GalleryPreviewSection images={previewGalleryImages} />
-      </PageTemplate>
-    </>
-  )
-}
+      </Grid>
+    </PageTemplate>
+  </>
+)
 
 export default Home
