bbb3361d7d0d573be5b5f70d3c55804fa6ba4f9e
Alan Morales <alanmoraales845@gmail.com>
2021-11-28 15:20:38 -0600

    change favicon and logo

--- src/components/atoms/icons/Logo.tsx
+++ src/components/atoms/icons/Logo.tsx
@@ -2,21 +2,14 @@ import { Icon, IconProps } from '@chakra-ui/react'
 
 // TODO use useTheme to change the #f9f9f9 color in circle fill
 const Logo = (props: IconProps) => (
-  <Icon color="primary.normal" viewBox="0 0 29 29" {...props}>
-    <circle cx="14.5" cy="14.5" r="12.5" fill="#f9f9f9" />
+  <Icon color="#A61A1A" viewBox="0 0 32 32" {...props}>
     <path
-      fill="none"
-      stroke="currentColor"
-      strokeMiterlimit="10"
-      strokeWidth="1.094"
-      d="M14.5 22.239a7.738 7.738 0 007.736-7.74A7.738 7.738 0 0014.5 6.762 7.737 7.737 0 006.764 14.5a7.737 7.737 0 007.736 7.739z"
+      d="M16 23.729a6.007 6.007 0 0 1-6-6v-6.457l6-3 6 3v6.456a6.007 6.007 0 0 1-6 6v.001Zm-4-11.222v5.222a4 4 0 1 0 8 0v-5.222l-4-2-4 2Z"
+      fill="currentColor"
     />
     <path
-      fill="none"
-      stroke="currentColor"
-      strokeMiterlimit="10"
-      strokeWidth="2.188"
-      d="M14.5 27C21.404 27 27 21.404 27 14.5S21.404 2 14.5 2 2 7.596 2 14.5 7.596 27 14.5 27z"
+      d="M16 31a1 1 0 0 1-.504-.136l-12-7A1 1 0 0 1 3 23V9a1 1 0 0 1 .496-.864l12-7a1 1 0 0 1 1.008 0l12 7-1.008 1.728L16 3.158 5 9.574v12.852l11 6.417 11-6.417V15h2v8a1 1 0 0 1-.496.864l-12 7A1 1 0 0 1 16 31Z"
+      fill="currentColor"
     />
   </Icon>
 )
